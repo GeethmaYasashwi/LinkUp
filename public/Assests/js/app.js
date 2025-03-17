66ec11248ff7dc026abf42907d94983331b90bf6
GeethmaYasashwi <geethmayasaswi@gmail.com>
2025-03-17 21:53:54 +0530

    Initial commit

---
+++ public/Assests/js/app.js
@@ -0,0 +1,13 @@
+var MYAPP = (function(){
+
+    function init(uid,mid)
+    {
+        
+
+    }
+    return{
+        _init:function(uid,mid){
+            Infinity(uid,mid);
+        }
+    }
+})
